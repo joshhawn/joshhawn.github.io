40a93414a6556b9e71367ebabd002d0ff7284466
Josh Hawn <hawn.jd@gmail.com>
2025-11-10 00:30:25 -0500

    updated structure to overall page
    added more consistent tags to sections and will divide appropriately. Input a form but need to connect it to google form/sheet.

--- js-portfolio.js
+++ js-portfolio.js
@@ -11,3 +11,12 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
+
+  <script src="assets/js/jquery.min.js"></script>
+<script type="text/javascript">var submitted=false;</script>
+<script type="text/javascript">
+$('#gform').on('submit', function(e) {
+  $('#gform *').fadeOut(2000);
+  $('#gform').prepend('Your submission has been processed...');
+  });
+</script>
